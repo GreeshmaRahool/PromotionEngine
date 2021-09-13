51a62af63028e8b417e8f3378b9f024e99461146
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-09-13 22:47:34 +0200

    Added cart items as objects

--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -7,6 +7,11 @@ namespace PromotionEngine
         static void Main(string[] args)
         {
             var cart = new Cart();
+
+            cart.Add(new CartItem { Sku = "A", Value = 50.0M }, 6);
+            cart.Add(new CartItem { Sku = "B", Value = 30.0M }, 3);
+            cart.Add(new CartItem { Sku = "C", Value = 20.0M }, 2);
+            cart.Add(new CartItem { Sku = "D", Value = 15.0M }, 3);
         }
     }
 }
