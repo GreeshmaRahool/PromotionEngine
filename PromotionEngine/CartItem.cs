b5d0c099427d34881ecd62ba74b6707204d45f08
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-09-13 21:50:59 +0200

    Added cart items class

---
+++ PromotionEngine/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class CartItem
+    {
+        public string Sku { get; set; }
+        public double Value { get; set; }
+
+        public override string ToString()
+        {
+            return "SKU: " + Sku + " " + "Value: " + Value;
+        }
+    }
+}
